fcbc6e82fd9528d6129d3b2f5614f1976b3ed50a
AleTornesello <alessandro.tornesello99@gmail.com>
2021-03-10 13:18:28 +0100

    feat: create connection section

--- pages/index.vue
+++ pages/index.vue
@@ -1,81 +1,65 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation </a
-            >.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord </a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br />
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+  <div>
+    <v-row>
+      <v-col cols="12">
+        <v-sheet class="grey lighten-4 rounded-lg pa-5 d-flex align-center">
+          <v-select
+            v-model="selectedBrokerId"
+            :items="brokers"
+            item-text="name"
+            item-value="id"
+            rounded
+            filled
+            hide-details
+            style="max-width: 420px"
+            class="mr-5"
+          ></v-select>
+          <v-btn rounded color="secondary" @click="connectToBroker()">
+            Connect
+          </v-btn>
+        </v-sheet>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
-export default {}
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  name: 'Dashboard',
+  data() {
+    return {
+      selectedBrokerId: null,
+
+      tabs: {
+        model: 'publish'
+      }
+    }
+  },
+  computed: {
+    ...mapGetters({
+      getBrokers: 'brokers/getBrokers'
+    }),
+    brokers() {
+      return this.getBrokers
+    }
+  },
+  mounted() {
+    if (this.brokers.length > 0) {
+      this.selectedBrokerId = this.brokers[0].id
+    }
+  },
+  methods: {
+    ...mapActions({
+      connectToBrokerStore: 'brokers/connectToBroker'
+    }),
+    connectToBroker() {
+      const brokerConfig = this.brokers.find(
+        (broker) => broker.id === this.selectedBrokerId
+      )
+      this.connectToBrokerStore(brokerConfig)
+    }
+  }
+}
 </script>
