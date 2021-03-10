007589315908038cd691fcfc6a4f07aed8f74bc8
AleTornesello <alessandro.tornesello99@gmail.com>
2021-03-10 11:43:07 +0100

    style: set light theme as default

--- nuxt.config.js
+++ nuxt.config.js
@@ -52,8 +52,17 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
+        light: {
+          primary: colors.blue.base,
+          accent: colors.blue.lighten1,
+          secondary: colors.blue.lighten1,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3,
+        },
         dark: {
           primary: colors.blue.darken2,
           accent: colors.grey.darken3,
