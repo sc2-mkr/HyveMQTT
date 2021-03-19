e73723b58c624867357bb2f2ded1726c9bfee83a
AleTornesello <alessandro.tornesello99@gmail.com>
2021-03-19 13:01:33 +0100

    feat: add notification component to default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,15 @@
 <template>
   <v-app dark>
+    <notifications group="generic">
+      <template #body="props">
+        <div :class="props.item.type" class="notification d-flex align-center">
+          <div class="flex-grow-1" v-html="props.item.text"></div>
+          <v-btn icon small color="white" @click="props.close">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </div>
+      </template>
+    </notifications>
     <Drawer></Drawer>
     <Appbar></Appbar>
     <v-main>
@@ -25,3 +35,31 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+.notification {
+  padding: 10px;
+  margin: 5px !important;
+
+  font-size: 14px !important;
+
+  color: #ffffff;
+  background: #44a4fc;
+  border-left: 5px solid #187fe7 !important;
+
+  &.warn {
+    background: #ffb648;
+    border-left-color: #f48a06 !important;
+  }
+
+  &.error {
+    background: #e54d42;
+    border-left-color: #b82e24 !important;
+  }
+
+  &.success {
+    background: #68cd86;
+    border-left-color: #42a85f !important;
+  }
+}
+</style>
